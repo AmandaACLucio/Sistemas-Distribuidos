5ba5ee79b4e6cd0763151def7f19066b510c0a2c
Amanda Lucio <amandalucio@poli.ufrj.br>
2022-05-23 02:00:13 -0300

    l

--- Projeto 2/2. Semaforo/Semaforo.c
+++ Projeto 2/2. Semaforo/Semaforo.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
 #include <time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
 #include <stdbool.h>
 #include <math.h>
+#include <pthread.h>
+#include <semaphore.h> 
 #define ERRO               1
 #define BUFFER_TAMANHO     20
 
