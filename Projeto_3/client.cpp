8305e6f3a34e5845d09becf484bf25e82edf7199
Amanda <amandalucio@poli.ufrj.br>
2022-07-21 11:46:49 -0300

    testes

--- Projeto_3/client.cpp
+++ Projeto_3/client.cpp
@@ -1,41 +1,48 @@
 #include <stdio.h> 
 #include <stdlib.h>
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
 #include <string.h> 
 #include <time.h>
 #include <sys/wait.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 #define PORT 54000 
 using namespace std;
 
 void writeFile(string client, string messages){
-// Create and open a text file
-  ofstream MyFile("log.txt");
+	// Create and open a text file
+	ofstream MyFile("log.txt");
 
-  // Write to the file
-  MyFile << client + messages;
+	// Write to the file
+	MyFile << client + messages;
 
-  // Close the file
-  MyFile.close();
+	// Close the file
+	MyFile.close();
 }
 
 string valueTimeinfo()
 {
-  time_t tmp = {0};
-  struct tm timeinfo = *localtime(&tmp);
-  return asctime (timeinfo);
+    time_t tmp;
+	time ( &tmp );
+	struct tm * timeinfo = localtime ( &tmp );
+	return asctime (timeinfo);
 }
 
 int connectSocket()
 {
 	int sock = 0; 
 	struct sockaddr_in serv_addr;
-	
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
 	{ 
-		cout<<<"Error creating socket"<<endl; 
+		cout<<"Error creating socket"<<endl; 
 		return -1; 
 	} 
 
@@ -45,29 +52,35 @@ int connectSocket()
 	// Convertion addresses to binary 
 	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
 	{ 
-		cout<<<"Invalid Address"<<endl; 
+		cout<<"Invalid Address"<<endl; 
 		return -1; 
 	} 
 	
 	while((connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)) 
 	{ 
-		cout<<<"Connection Failed"<<endl; 
+		cout<<"Connection Failed"<<endl; 
+		cout<<sock<<"  "<<(struct sockaddr *)&serv_addr<<endl;
+
+		int a;
+		cin>>a;
 
 	}
+
+	cout<<"sock"<<sock<<endl;
 	return sock;
 }
 
-int request(char* buffer, char* PIDserver, int socket)
+int request(char* buffer, char* PIDserver, int sock)
 {
 	int valueRead;
 
 	strncpy(buffer, "1|\0", 1024);
 	strcat(buffer,PIDserver);
 
-	dprintf(1,"Enviando mensagem %s\n",buff);
+	dprintf(1,"Enviando mensagem %s\n",buffer);
 
-	send(socket , buffer , 1024 , 0 ); 
-	valueRead = read(socket , buffer, 1024);
+	send(sock , buffer , 1024 , 0 ); 
+	valueRead = read(sock , buffer, 1024);
 
 	if(valueRead == 0 )
 	{	
@@ -82,65 +95,74 @@ int request(char* buffer, char* PIDserver, int socket)
 	return 0;
 }
 
-void release(char* buffer,char* PIDserver,int socket)
+void release(char* buffer,char* PIDserver,int sock)
 {
 	strncpy(buffer, "3|\0",1024);
 	strcat(buffer, PIDserver);
 
 	dprintf(1,"Enviando mensagem %s\n",buffer);
     
-	send(socket , buffer , 1024 , 0 );
+	send(sock , buffer , 1024 , 0 );
 	dprintf(1,"Release Critic Region\n"); 	
 }
 
-void createRequests(int countProcess,char* buffer,char* PIDserver,int socket,int pointerBuffer)
+void createRequests(int countProcess,char* buffer,char* PIDserver,int sock,int pointerBuffer)
 {
 	int sucess;
 	for(int j=0;j<countProcess;j++)	
 	{
-		sucess = request(buffer,PIDserver,socket);
+		sucess = request(buffer,PIDserver,sock);
 		if(sucess)
 		{
-			writeFile(PIDserver, pointerBuffer);
-			release(buffer, PIDserver, socket);
+			writeFile(PIDserver, to_string(pointerBuffer));
+			release(buffer, PIDserver, sock);
 		}
 	}
-	close(socket);
+	close(sock);
 }
 
 int main(int argc, char const *argv[]) 
 { 
+	if(argc!=4){
+		cout<<"Insira os parâmetros necessários:"<<endl;
+		cout<<"./client n r k"<<endl;
+		return 0;
+	}
+
     int n = atoi(argv[1])-1; //number of process
 	int r = atoi(argv[2]); //number of repetitions
   	int k = atoi(argv[3]); //tempo de espera
-  	int *pids = malloc(n*sizeof(int));
+
+	vector<int> pids;
+  	//int *pids = malloc(n*sizeof(int));
 
 	for (int i = 0; i < n; i++)
   	{
-    		if ((pids[i] = fork()) < 0)
-    		{
-    			exit(1);
-    		}
-    		else 
-    		{
-    			int process_id = getpid();
-                
-                char PIDserver[1024];
-                char buffer[1024] = {0};
-                
-                sprintf(PIDserver,"%d|",process_id); 
-                
-                int socket = connectSocket();
-                createRequests(r,buffer,PIDserver,socket,k);
-    		}
-    	}
+		try{
+			pids.push_back(fork());
+		}catch(int e){
+			exit(1);
+		}
+
+		int process_id = getpid();
+		
+		char PIDserver[1024];
+		char buffer[1024] = {0};
+		
+		sprintf(PIDserver,"%d|",process_id); 
+		
+		int socket = connectSocket();
+		cout<<"Conexão bem sucedida"<<endl;
+		createRequests(r,buffer,PIDserver,socket,k);
+
+	}
 
     int response;
-	int pid;
+	//int pid;
     
 	while (n > 0)
 	{
-		pid = wait(&response);
+		wait(&response);
 		--n;
 	}
 	return 0; 
